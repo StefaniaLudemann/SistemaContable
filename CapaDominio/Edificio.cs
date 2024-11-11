3ec04fcd21322a13443730b3b58c40d204207273
Facundo-Banegaz <Banegaz@users.noreply.github.com>
2024-11-11 16:18:38 -0300

    se agregaron las clases unidad y usuario

--- CapaDominio/Edificio.cs
+++ CapaDominio/Edificio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace CapaDominio
 {
     public class Edificio
     {
+        //atributos
+        public int Id { get; set; }
+
+        [DisplayName("Nombre")]
+        public string Nombre { set; get; }
+
+
+        [DisplayName("Dirección")]
+        public string Direccion { set; get; }
+
+        //relacion 1:n
+
+        public ICollection<Unidad> Unidades { set; get; }
+
+        public ICollection<Egreso> Egresos { set; get; }
     }
 }
