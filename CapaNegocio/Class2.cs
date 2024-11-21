df832cb32a18585f029e4adf332289059af891b8
steffi lude <m.houdini86@gmail.com>
2024-11-21 18:05:43 -0300

    se creo class2 en capa negocio

---
+++ CapaNegocio/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    internal class Class2
+    {
+        //creacion class2 para verificar correcto funcionamiento de la colaboracion
+    }
+}
